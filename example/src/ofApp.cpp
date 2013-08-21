0483f43ed90657e35f2d23dcc6f44fe9db020658
Christopher Baker <me@christopherbaker.net>
2013-08-21 14:28:22 -0500

    Example updates.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,8 +1,35 @@
-#include "exampleApp.h"
+// =============================================================================
+//
+// Copyright (c) 2009-2013 Christopher Baker <http://christopherbaker.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// =============================================================================
 
 
-//--------------------------------------------------------------
-void exampleApp::setup(){
+#include "ofApp.h"
+
+
+//------------------------------------------------------------------------------
+void ofApp::setup()
+{
+
     instructions = "Drag file onto window for more info.";
     mediaType = "";
 
@@ -12,8 +39,8 @@ void exampleApp::setup(){
     ofNotifyDragEvent(simulatedDrag);
 }
 
-//--------------------------------------------------------------
-void exampleApp::draw(){
+//------------------------------------------------------------------------------
+void ofApp::draw(){
     ofBackground(0);
 
     // draw instructions and info
@@ -22,10 +49,11 @@ void exampleApp::draw(){
     ofDrawBitmapString(mediaType, 10, 75);
 }
 
-//--------------------------------------------------------------
-void exampleApp::dragEvent(ofDragInfo dragInfo) {
+//------------------------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
-    if(!dragInfo.files.empty()) {
+    if(!dragInfo.files.empty())
+    {
         file = Poco::File(dragInfo.files[0]);
         mediaType = mediaMap.getMediaTypeForFile(file).toString();
     }
